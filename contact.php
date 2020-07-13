98dbb7ab96f6777f86c8ad0cfc20b0d896da95b3
yunuswidodo <younews372@gmail.com>
2020-07-13 09:50:49 +0700

    terakhir kecuali database

---
+++ contact.php
@@ -0,0 +1,16 @@
+<?php
+include "_header.html";
+?>
+<div class="card bg-dark text-white text-center">
+    <img src="img/telepon.jpg" class="card-img" alt="...">
+    <div class="card-img-overlay">
+        <br>
+        <h5 class="card-title">contact</h5>
+        <p class="card-text">jika menemui kendala hubungi admin dengan nomor wa: 082242842127</p>
+        <p class="card-text">sekian trimakasih apa bila ada kesalahan mohon dimaafkan </p>
+    </div>
+</div>
+
+<?php
+include "_footer.html";
+?>
